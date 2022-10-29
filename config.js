5400dd3ec4a57ecf0950b3130a9c1be6c1e8af40
killbasa <killbasab@gmail.com>
2022-10-29 02:43:36 -0400

    Fix config for ci/cd

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    discord: {
+        token: process.env.DISCORD_TOKEN,
+        id: process.env.CLIENT_ID,
+        secret: process.env.CLIENT_SECRET,
+    },
+    api: {
+        port: process.env.API_PORT
+    },
+    metrics: {
+        ports: process.env.METRICS_PORT
+    }
+}
+
+module.exports = config;
