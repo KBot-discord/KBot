1788a53c1ae8cc3a0ae4b1b1851f22a868ca46e7
killbasa <killbasab@gmail.com>
2022-10-30 22:24:04 -0400

    Add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
+// Imports
 import '@sapphire/plugin-api/register';
 import '@sapphire/plugin-logger/register';
-import {container, LogLevel} from "@sapphire/framework";
-
-import {KBotClient} from "./lib/KBotClient";
-import {Config} from "./lib/types/config";
+import { container, LogLevel } from "@sapphire/framework";
+import { RewriteFrames } from '@sentry/integrations';
+import * as Sentry from '@sentry/node';
+import { KBotClient } from "./lib/extensions/KBotClient";
+import { Config } from "./lib/types/config";
 import startMetricsServer from "./lib/util/metrics";
+import { rootFolder } from "./lib/util/constants";
 
 
 const config: Config = require('../config.js');
+// if (config is not valid) process.exit(1);
 container.config = config;
 
 const client = new KBotClient({
@@ -16,7 +20,6 @@ const client = new KBotClient({
         'GUILD_MEMBERS',
         'GUILD_VOICE_STATES',
         'GUILD_SCHEDULED_EVENTS',
-        'GUILD_MESSAGE_REACTIONS',
     ],
     api: {
         listenOptions: {
@@ -24,16 +27,33 @@ const client = new KBotClient({
         },
     },
     logger: {
-        level: process.env.NODE_ENV === 'production' ? LogLevel.Info : LogLevel.Debug,
+        level: config.isDev ? LogLevel.Debug : LogLevel.Info,
     }
 });
 
 async function main() {
+    if (config.sentry.enable) {
+        Sentry.init({
+            dsn: config.sentry.dsn,
+            integrations: [
+                new Sentry.Integrations.Modules(),
+                new Sentry.Integrations.FunctionToString(),
+                new Sentry.Integrations.LinkedErrors(),
+                new Sentry.Integrations.Console(),
+                new Sentry.Integrations.Http({ breadcrumbs: true, tracing: true }),
+                new RewriteFrames({ root: rootFolder })
+            ]
+        });
+    }
+
     try {
         startMetricsServer();
+        // container.db = await connect();
         await client.login(config.discord.token);
-    } catch {
+    } catch (error) {
+        container.logger.error(error);
         await client.destroy();
+        process.exit(1);
     }
 }
 
