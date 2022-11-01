e6293a678a5811896330794e1a409c7e60fb05f7
killbasa <killbasab@gmail.com>
2022-11-01 01:00:00 -0400

    Extend sapphire client

---
+++ src/lib/extensions/KBotClient.ts
@@ -0,0 +1,37 @@
+// Imports
+import {container, SapphireClient } from '@sapphire/framework';
+import { DatabaseClient } from "../database/DatabaseClient";
+import { RedisClient } from "../redis/RedisClient";
+
+// Types
+import type { ClientOptions } from "discord.js";
+import type { Config } from "../types/config";
+import type { Counters } from "../types/client";
+
+
+export class KBotClient extends SapphireClient {
+    public constructor(options: ClientOptions) {
+        super(options);
+        container.db = new DatabaseClient(container.config.db.url)
+        container.redis = new RedisClient();
+    }
+
+    public override async login(token: string) {
+        return super.login(token);
+    }
+
+    public override async destroy() {
+        await container.db.$disconnect();
+        container.redis.disconnect();
+        return super.destroy();
+    }
+}
+
+declare module '@sapphire/pieces' {
+    interface Container {
+        db: DatabaseClient;
+        redis: RedisClient;
+        config: Config;
+        counters: Counters;
+    }
+}
