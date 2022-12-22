8f4bd89a648df6cd531a1718ac258132cfd63f17
killbasa <killbasab@gmail.com>
2022-12-22 18:04:08 -0500

    Add caching to modules

--- src/modules/ModerationModule.ts
+++ src/modules/ModerationModule.ts
@@ -1,20 +1,33 @@
 import { ChatInputModuleCommand, Module } from '@kbotdev/plugin-modules';
 import { container } from '@sapphire/framework';
-import { isNullish } from '@sapphire/utilities';
 import type { Guild } from 'discord.js';
+import type { ModerationModule as ModerationModel } from '@prisma/client';
+import type { Key } from '../lib/types/Cache';
 
 export class ModerationModule extends Module {
 	private readonly db;
+	private readonly cache;
+
+	private readonly cacheKey = (guildId: string) => `kbot:core:guilds:${guildId}:moderation:enabled` as Key;
 
 	public constructor(context: Module.Context, options: Module.Options) {
 		super(context, { ...options });
 		this.db = container.db.moderationModule;
+		this.cache = container.redis;
 	}
 
 	public async isEnabled(_command: ChatInputModuleCommand, guild: Guild): Promise<boolean> {
-		const result = await this.db.findUnique({ where: { id: guild.id } });
-		if (isNullish(result)) return false;
-		return result.moduleEnabled;
+		const key = this.cacheKey(guild.id);
+		const cacheResult = await this.cache.get<ModerationModel>(key);
+		if (cacheResult) {
+			await this.cache.update(key, 5);
+			return cacheResult.moduleEnabled;
+		}
+
+		const dbResult = await this.db.findUnique({ where: { id: guild.id } });
+		if (!dbResult) return false;
+
+		return dbResult.moduleEnabled;
 	}
 }
 
--- src/modules/UtilityModule.ts
+++ src/modules/UtilityModule.ts
@@ -1,25 +1,38 @@
 import { ChatInputModuleCommand, Module } from '@kbotdev/plugin-modules';
 import { container } from '@sapphire/framework';
-import { isNullish } from '@sapphire/utilities';
 import type { Guild } from 'discord.js';
+import type { Key } from '../lib/types/Cache';
+import type { UtilityModule as UtilityModel } from '@prisma/client';
 
 export class UtilityModule extends Module {
 	private readonly db;
+	private readonly cache;
+
+	private readonly cacheKey = (guildId: string) => `kbot:core:guilds:${guildId}:utility:enabled` as Key;
 
 	public constructor(context: Module.Context, options: Module.Options) {
 		super(context, { ...options });
 		this.db = container.db.utilityModule;
+		this.cache = container.redis;
 	}
 
 	public async isEnabled(_command: ChatInputModuleCommand, guild: Guild): Promise<boolean> {
-		const result = await this.db.findUnique({ where: { id: guild.id } });
-		if (isNullish(result)) return false;
-		return result.moduleEnabled;
+		const key = this.cacheKey(guild.id);
+		const cacheResult = await this.cache.get<UtilityModel>(key);
+		if (cacheResult) {
+			await this.cache.update(key, 5);
+			return cacheResult.moduleEnabled;
+		}
+
+		const dbResult = await this.db.findUnique({ where: { id: guild.id } });
+		if (!dbResult) return false;
+
+		return dbResult.moduleEnabled;
 	}
 }
 
 declare module '@kbotdev/plugin-modules' {
 	interface Modules {
-		ModerationModule: never;
+		UtilityModule: never;
 	}
 }
