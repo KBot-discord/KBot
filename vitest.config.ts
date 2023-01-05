36dcb246361aa49b92f58c771b37b52908a13e46
killbasa <killbasab@gmail.com>
2023-01-05 01:51:59 -0500

    Enable coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		root: './',
-		include: ['./tests/**/*.test.ts'],
-		setupFiles: './tests/vitest.setup.ts',
-		reporters: ['default', 'junit'],
-		outputFile: './coverage/junit.xml',
 		coverage: {
-			provider: 'istanbul'
+			reporter: ['text', 'lcov', 'clover']
 		}
 	}
 });
