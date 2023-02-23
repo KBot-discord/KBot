55628657fe9da6c974fb53dfdebc2e18eed6c374
killbasa <killbasab@gmail.com>
2023-02-22 20:42:13 -0500

    Add placeholder test

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,15 @@ export default defineConfig({
 		globals: true,
 		coverage: {
 			reporter: ['text', 'lcov', 'clover']
-		}
+		},
+		exclude: [
+			'**/node_modules/**',
+			'**/dist/**',
+			'**/cypress/**',
+			'**/.{idea,git,cache,output,temp}/**',
+			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+			'**/infra',
+			'**/prisma/seed'
+		]
 	}
 });
