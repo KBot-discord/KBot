a92eb5bd56a4f718410f318747544a7080c19ff2
Shane <killbasab@gmail.com>
2023-03-22 18:39:18 -0400

    fix(bot): check role position for youtube (#34)

--- apps/bot/src/interaction-handlers/select-menu/youtubeRoleReaction.ts
+++ apps/bot/src/interaction-handlers/select-menu/youtubeRoleReaction.ts
@@ -2,7 +2,7 @@ import { YoutubeCustomIds } from '#utils/customIds';
 import { ApplyOptions } from '@sapphire/decorators';
 import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework';
 import { isNullish } from '@sapphire/utilities';
-import { PermissionFlagsBits } from 'discord.js';
+import { PermissionFlagsBits, roleMention } from 'discord.js';
 import type { StringSelectMenuInteraction } from 'discord.js';
 
 @ApplyOptions<InteractionHandler.Options>({
@@ -19,26 +19,53 @@ export class ButtonHandler extends InteractionHandler {
 			guildId: interaction.guildId
 		});
 
+		const invalidRoleIds: Set<string> = new Set();
 		const userRoles = interaction.member.roles.cache;
+		const bot = await interaction.guild.members.fetchMe();
 
 		const roleIds = subscriptions //
 			.filter(({ roleId, memberRoleId }) => (member ? !isNullish(memberRoleId) : !isNullish(roleId)))
 			.map(({ channel, roleId, memberRoleId }) => ({ channel, roleId: member ? memberRoleId : roleId }));
+
+		for (const { roleId } of roleIds) {
+			const role = await interaction.guild.roles.fetch(roleId!);
+			if (role && bot.roles.highest.position <= role.position) {
+				invalidRoleIds.add(role.id);
+			}
+		}
 
 		const selectedRoles = roleIds //
 			.filter(({ channel }) => selectedChannels.includes(channel.youtubeId));
 
 		const rolesToAdd = new Set<string>(
 			selectedRoles //
-				.filter(({ roleId }) => !userRoles.has(roleId!))
+				.filter(
+					({ roleId }) =>
+						!userRoles.has(roleId!) && //
+						!invalidRoleIds.has(roleId!)
+				)
 				.map(({ roleId }) => roleId!)
 		);
 
 		const rolesToRemove = new Set<string>(
 			roleIds //
-				.filter(({ roleId }) => userRoles.has(roleId!) && !selectedRoles.some((sub) => sub.roleId === roleId))
+				.filter(
+					({ roleId }) =>
+						userRoles.has(roleId!) && //
+						!selectedRoles.some((sub) => sub.roleId === roleId) &&
+						!invalidRoleIds.has(roleId!)
+				)
 				.map(({ roleId }) => roleId!)
 		);
+
+		if (invalidRoleIds.size > 0) {
+			await interaction.errorFollowup(
+				`I was not able to add/remove the following roles due to them being higher than my highest role:\n\n${[...invalidRoleIds.values()]
+					.map((roleId) => roleMention(roleId))
+					.join(' ')}`,
+				true
+			);
+		}
 
 		await interaction.member.roles.add([...rolesToAdd.values()]);
 		await interaction.member.roles.remove([...rolesToRemove.values()]);
