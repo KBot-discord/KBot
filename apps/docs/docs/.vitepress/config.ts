bb76627f3a3ce702c7f487d0fd37fdb54673171c
Shane <killbasab@gmail.com>
2023-03-30 14:19:50 -0400

    Change doc workflow action (#42)

--- apps/docs/docs/.vitepress/config.ts
+++ apps/docs/docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ const plusIcon =
 const links: { url: string; lastmod: number | undefined }[] = [];
 
 export default defineConfig({
-	base: "/",
+	base: "/kbot/",
 	lang: "en-US",
 	title: "KBot Documentation",
 	description: "Documentation for KBot",
