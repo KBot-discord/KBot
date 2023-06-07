487836f0e79b38f5203f4587dafd98a68f4bd703
Shane <killbasab@gmail.com>
2023-06-07 17:19:07 -0400

    chore: bump versions (#126)
    * chore: bump versions
    
    * chore: remove config from labeler

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	root: true,
-	extends: ['@kbotdev/eslint-config'],
+	extends: ['@kbotdev/eslint-config', 'turbo'],
 	parserOptions: {
 		project: './tsconfig.eslint.json',
 		tsconfigRootDir: __dirname
