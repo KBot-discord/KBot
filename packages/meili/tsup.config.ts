34ec5310d0803e5375c81fde039b776cf4f9f510
Shane <killbasab@gmail.com>
2023-12-29 08:25:47 -0500

    fix: monorepos suck (#201)

--- packages/meili/tsup.config.ts
+++ packages/meili/tsup.config.ts
@@ -1,19 +1,11 @@
-import { defineTsupConfig } from '../../scripts/tsup';
-import { relative } from 'node:path';
+import { defineConfig } from 'tsup';
 
-export default defineTsupConfig({
-	bundle: true,
+export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ['src/**/*.ts', '!src/**/*.d.ts'],
+	entry: ['src/index.ts'],
 	format: ['esm'],
-	keepNames: true,
 	minify: true,
-	shims: false,
-	skipNodeModulesBundle: true,
-	splitting: false,
 	sourcemap: true,
-	target: 'es2022',
-	treeshake: true,
-	tsconfig: relative(__dirname, './tsconfig.json')
+	tsconfig: 'tsconfig.json'
 });
